ed61639ae6da69038a897e0ebf4a8dcddea44353
Kilox <qianxianqwq@163.com>
2025-06-18 12:43:08 +0800

    feat: implement authentication flow with user state management and route protection

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,16 +1,17 @@
-import { useSignOut } from "react-firebase-hooks/auth";
+import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
 import { auth } from "../firebase/firebase";
-import { useShowToast } from "./useShowToast";
+import useShowToast from "./useShowToast";
 
 const useLogout = () => {
   const [signOut, isLoggingOut, error] = useSignOut(auth);
   const showToast = useShowToast();
+  const logoutUser = useAuthState((state) => state.logout);
 
   const handleLogout = async () => {
     try {
       await signOut();
       localStorage.removeItem("user-info");
-      console.log("logged out");
+      logoutUser();
     } catch (error) {
       showToast("Error", error.message, "error");
     }
